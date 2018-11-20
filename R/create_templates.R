66e6b696c89801f0f59bc01ac22278ec1e21e667
Grant Innerst <grantinnerst@Grants-MacBook-Pro.local>
2018-11-20 15:20:37 -0600

    cleanup function output, improve documentation

--- R/create_templates.R
+++ R/create_templates.R
@@ -2,28 +2,76 @@
 #'
 #' This function creates a specified directory and populates the directory with the knitr template.
 #'
-#' @param direc path to a directory (not yet created) where the template will reside
+#' @param direc path to a directory where the template will reside. If the directory does not exist, the function will create it.
 #' @return nothing
 #' @export create_knitr_thesis
 
 
 create_knitr_thesis <- function(direc) {
-  current_template_loc <- system.file("knitr_template", package = "buthesis")
-  dir.create(direc)
-  file.copy(list.files(current_template_loc, full.names = T), direc)
+  # find the template on your file system
+  current_template_loc <-
+    system.file("knitr_template", package = "buthesis")
+
+  # if the directory does not exist, create it!
+
+  if(!dir.exists(direc)) {
+    dir.create(direc)
+  }
+
+  # copy the files to the new directory
+  did_copy <-
+    capture.output(file.copy(list.files(current_template_loc, full.names = T), direc))
+
+  # error checking for copying process
+
+  # process captured output from file.copy
+  did_copy <- unlist(strsplit(did_copy, " "))
+
+  if (all(did_copy[-1] == "TRUE")) {
+    message("Template copied succesfully")
+  } else {
+    message(
+      "Template may not have been copied successfully. Please make sure your directory
+      matches the template on GitHub found at https://github.com/GrantInnerst/buthesis"
+    )
+  }
 }
 
 
 #' Create a directory that contains the proper Baylor Statistical Science thesis latex template
 #'
-#' This function creates a specified directory and populates the directory with the knitr template.
+#' This function creates a specified directory and populates the directory with the latex template.
 #'
-#' @param direc path to a directory (not yet created) where the template will reside
+#' @param direc path to a directory where the template will reside. If the directory does not exist, the function will create it.
 #' @return nothing
 #' @export create_latex_thesis
 
 create_latex_thesis <- function(direc) {
-  current_template_loc <- system.file("latex_template", package = "buthesis")
-  dir.create(direc)
-  file.copy(list.files(current_template_loc, full.names = T), direc)
+  # find the template on your file system
+  current_template_loc <-
+    system.file("latex_template", package = "buthesis")
+
+  # if the directory does not exist, create it!
+
+  if(!dir.exists(direc)) {
+    dir.create(direc)
+  }
+
+  # copy the files to the new directory
+  did_copy <-
+    capture.output(file.copy(list.files(current_template_loc, full.names = T), direc))
+
+  # error checking for copying process
+
+  # process captured output from file.copy
+  did_copy <- unlist(strsplit(did_copy, " "))
+
+  if (all(did_copy[-1] == "TRUE")) {
+    message("Template copied succesfully")
+  } else {
+    message(
+      "Template may not have been copied successfully. Please make sure your directory
+      matches the template on GitHub found at https://github.com/GrantInnerst/buthesis"
+    )
+  }
 }
